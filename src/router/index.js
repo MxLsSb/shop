2cc2a7cf6da0dcb672172dfd2fbc72669d0ebb97
MxLsSb <you@example.com1069236447@qq.com>
2019-11-27 17:27:59 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Category from '../views/Category.vue'
+import Cart from '../views/Cart.vue'
+import Profile from '../views/Profile.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }, {
+    path: '/Category',
+    name: 'Category',
+    component: Category
+  },
+  {
+    path: '/Cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/Profile',
+    name: 'Profile',
+    component: Profile
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
